0c303fe710f1982ad839d04bf4a66a7ce605d4a4
Akagra-Mish <71135222+Akagra-Mish@users.noreply.github.com>
2021-01-21 11:42:33 +0530

    Chatting-Era Source Code

---
+++ app/src/main/java/com/techzgamecoding/chatting_era/Activities/Settings.java
@@ -0,0 +1,16 @@
+package com.techzgamecoding.chatting_era.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techzgamecoding.chatting_era.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
